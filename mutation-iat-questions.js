7fdba1dfc8b19e4e3e76252e005fe017105e3032
Zachary Gildersleeve <31549539+gildersleeve@users.noreply.github.com>
2021-06-28 15:20:42 -0600

    Added some words

--- mutation-iat-questions.js
+++ mutation-iat-questions.js
@@ -36,7 +36,51 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
     			{image : 'ym5.jpg'}			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
+		},
+		
+		attribute2 :
+		{
+			name : 'Good words',
+			title : {
+				media : {word : 'Good words'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'laughter'},
+				{word: 'happy'},
+				{word: 'glorious'},
+				{word: 'joy'},
+				{word: 'wonderful'},
+				{word: 'peace'},
+				{word: 'pleasure'},
+				{word: 'love'}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		attribute1 :
+		{
+			name : 'Bad words',
+			title : {
+				media : {word : 'Bad words'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'awful'},
+				{word: 'failure'},
+				{word: 'agony'},
+				{word: 'hurt'},
+				{word: 'horrible'},
+				{word: 'terrible'},
+				{word: 'nasty'},
+				{word: 'evil'}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},		
+		
 
 		base_url : {//Where are your images at?
 			image : 'https://baranan.github.io/minno-tasks/images/'
